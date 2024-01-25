c7afc8a846ee025187992f8915688fdee16ba459
Allon Nam <allon.nam96@gmail.com>
2024-01-25 18:22:24 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import About from '@/components/About';
+import ContactMe from '@/components/ContactMe';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
+import WorkExperience from '@/components/WorkExperience';
+import type { NextPage } from 'next'
+import Head from "next/head";
+import Link from 'next/link';
+
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20
+    scrollbar-thumb-[#F7AB0A]/70'>
+      <Head>
+        <title>Allon's Portfolio</title>
+      </Head>
+
+      <Header />
+
+      <section id='hero' className='snap-start'>
+        <Hero />
+      </section>
+
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
+
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
+
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
+
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
+
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
+
+      <Link href="#hero">
+      <footer className='sticky bottom-5 w-full cursor-pointer'>
+        <div className='flex items-center justify-center'>
+          <img 
+          className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+          src="https://w7.pngwing.com/pngs/319/880/png-transparent-two-black-upward-arrows-art-logo-graphic-design-up-arrow-angle-web-design-text.png">
+          </img>
+        </div>
+      </footer>
+      
+      </Link>
+
+    </div>
+  );
+};
+
+export default Home;
